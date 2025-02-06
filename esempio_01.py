8ee6ec28d530ccdb30fca057415d26b60b91c4bc
massimoandrea76 <massimoandrea.energia@gmail.com>
2025-02-06 10:15:40 +0100

    Add files via upload

---
+++ esempio_01.py
@@ -0,0 +1,12 @@
+somma = 0.0
+valore_precedente = 9999.99
+while True :
+    valore_corrente=float(input())
+    somma += valore_corrente
+    if valore_corrente == 0.0 and valore_precedente == 0.0
+        break
+    print("Sto per assegnare il valore corrente",
+           valore_corrente,
+           "rimpiazzando il valore precedente" ,
+           valore_precedente)
+    valore_precedente = valore_corrente
